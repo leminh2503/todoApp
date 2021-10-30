97e7167098de465f2bd34be526d345ed8307e3fc
minhpm123 <minhugvlog@gmail.com>
2021-10-30 07:33:29 +0700

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import TodoList from './compoment/TodoList/TodoList'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() { 
+  return (
+    <div className="App" style={{ padding: 32 }}>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
